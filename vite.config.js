34b0951fee0f5ab743a2d69b5ace1d48c0f455db
MicaelMarcos13 <mmjdamaceno@gmail.com>
2025-11-24 20:40:35 -0300

    Ignorando node_modules, dist e cache do gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dev-cadastro-novo/'   // 🔥 IMPORTANTE
+  base: '/dev-cadastro-novo/'   
 })
